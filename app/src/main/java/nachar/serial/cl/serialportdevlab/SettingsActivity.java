a3561fc651620013e99c87f5724da6007df43798
Gustavo Canales <gustavo.canaleslopez@gmail.com>
2016-07-11 02:20:13 -0400

    Añadido Settings y Campos

---
+++ app/src/main/java/nachar/serial/cl/serialportdevlab/SettingsActivity.java
@@ -0,0 +1,13 @@
+package nachar.serial.cl.serialportdevlab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
